33351b51885f995b15c23d709595957b27a13b89
Eduardo Matheus Vaz Gonçalves <116606461+WarlynXore@users.noreply.github.com>
2023-10-27 23:49:32 -0300

    Add files via upload

---
+++ atitusound/src/main/java/br/edu/atitus/pooavancado/atitusound/controllers/PlaylistController.java
@@ -0,0 +1,37 @@
+package br.edu.atitus.pooavancado.atitusound.controllers;
+
+import br.edu.atitus.pooavancado.atitusound.entities.PlaylistEntity;
+import br.edu.atitus.pooavancado.atitusound.entities.dtos.PlaylistDTO;
+import br.edu.atitus.pooavancado.atitusound.services.GenericService;
+import br.edu.atitus.pooavancado.atitusound.services.PlaylistService;
+
+public class PlaylistController extends GenericController<PlaylistEntity, PlaylistDTO>{
+
+	private final PlaylistService service;
+	
+	public PlaylistController(PlaylistService service) {
+		super();
+		this.service = service;
+	}
+
+	@Override
+	protected GenericService<PlaylistEntity> getService() {
+		return service;
+	}
+
+	protected PlaylistEntity convertdto2Entity(PlaylistDTO dto) {
+		PlaylistEntity entidade = new PlaylistEntity();
+		entidade.setName(dto.getName());
+		entidade.setPublic_share(dto.getPublic_share());
+
+		
+		return entidade;
+	}
+
+	@Override
+	PlaylistEntity convertDTO2Entity(PlaylistDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
